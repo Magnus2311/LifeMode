72eca068deaf707f43403835209c07cd51f52858
Yavor Orlyov <yavor.orlyov@microinvest.net>
2020-08-07 17:40:55 +0300

    #2

---
+++ src/AppRouter.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Switch, Route } from "react-router";
+import Home from "./components/Home";
+
+function AppRouter() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </Switch>
+  );
+}
+
+export default AppRouter;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { createBrowserHistory } from "history";
+import { Router } from "react-router";
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={history}>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
