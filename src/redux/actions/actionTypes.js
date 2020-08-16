f31fad4a004019194ede67f9c96986d2df79fdd3
Iavor Orlyov <iavor_orlyov@yahoo.com>
2020-08-16 16:00:59 +0300

    #14

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,1 @@
+export const LOAD_GOODS_SUCCESS = "LOAD_GOODS_SUCCESS";
--- src/redux/reducers/goodsReducers.js
+++ src/redux/reducers/goodsReducers.js
@@ -1,9 +1,9 @@
+import * as actionTypes from "../actions/actionTypes";
+
 export default function goodsReducer(goods = [], action) {
   switch (action.type) {
-    case "CREATE_GOODS":
-      return [...goods, goods.push(action.goods)];
-    case "SAVE_GOODS_SUCCESS":
-      return [...goods, goods.push(action.goods)];
+    case actionTypes.LOAD_GOODS_SUCCESS:
+      return action.LOAD_GOODS_SUCCESS;
     default:
       return goods;
   }
