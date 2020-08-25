14477a60681d2e0eae93c283a54bf4b30b3cb67b
Yavor Orlyov <yavor.orlyov@microinvest.net>
2020-08-25 15:22:30 +0300

    #26

--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { createBrowserHistory } from "history";
 import { BrowserRouter as Router } from "react-router-dom";
 import configureStore from "./redux/configureStore";
 import { Provider as ReduxProvider } from "react-redux";
 
-const history = createBrowserHistory();
 // TO DO initialize store on create with data from DB
 const store = configureStore();
 
 ReactDOM.render(
   <ReduxProvider store={store}>
     <React.StrictMode>
-      <Router history={history}>
+      <Router>
         <App />
       </Router>
     </React.StrictMode>
