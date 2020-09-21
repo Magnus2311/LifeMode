05fb07c21add25db5e745200baa055cd4243c704
Iavor Orlyov <iavor_orlyov@yahoo.com>
2020-09-21 17:13:09 +0300

    Moved cards to product

--- src/components/common/Categories/Category.js
+++ src/components/common/Categories/Category.js
@@ -1,68 +1,24 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import { Translator } from "../../../services/languages/Laguage";
-import { Link } from "react-router-dom";
 
 const Category = (props) => {
   const { handleClick } = props;
-  const { image, name, id, subCategories } = props.category;
+  const { image, name, id } = props.category;
 
   return (
-    // <Card
-    //   data-id={id}
-    //   className="category-item text-center"
-    //   onClick={handleClick}
-    // >
-    //   <Card.Body>
-    //     <Card.Img src={image}></Card.Img>
-    //     <Card.Header>
-    //       <Translator getString={name} />
-    //     </Card.Header>
-    //   </Card.Body>
-    // </Card>
-    <div className="category">
-      <div className="rank">{id}</div>
-      <div className="front">
-        <img className="thumbnail" src={image} alt="" />
-        <h3 className="name">{<Translator getString={name} />}</h3>
-        <div className="stats">
-          <p className="subcategories-count">{subCategories.length}</p>
-          <div className="subcategories">
-            {subCategories[0] && <img src={subCategories[0].image} alt="" />}
-            {subCategories[1] && <img src={subCategories[1].image} alt="" />}
-            {subCategories[2] && <img src={subCategories[2].image} alt="" />}
-          </div>
-        </div>
-      </div>
-
-      <div className="back">
-        <div className="category-info">
-          <p className="product-info">
-            <span>750</span>
-          </p>
-          <p className="product-info">
-            <span>750</span>
-          </p>
-        </div>
-        <div className="btn btn-primary btn-xl" onClick={handleClick} />
-        <div className="subcategories-back">
-          <div className="subcategory">
-            <p className="name"></p>
-            <p className="number"></p>
-          </div>
-          <div className="subcategory">
-            <p className="name"></p>
-            <p className="number"></p>
-          </div>
-          <div className="subcategory">
-            <p className="name"></p>
-            <p className="number"></p>
-          </div>
-        </div>
-      </div>
-
-      <div className="background"></div>
-    </div>
+    <Card
+      data-id={id}
+      className="category-item text-center"
+      onClick={handleClick}
+    >
+      <Card.Body>
+        <Card.Img src={image}></Card.Img>
+        <Card.Header>
+          <Translator getString={name} />
+        </Card.Header>
+      </Card.Body>
+    </Card>
   );
 };
 
--- src/components/pages/Goods/ProductPreview.js
+++ src/components/pages/Goods/ProductPreview.js
@@ -1,45 +1,98 @@
 import React from "react";
-import { Card, Container, Row } from "react-bootstrap";
 import { Translator } from "../../../services/languages/Laguage";
 
 const ProductPreview = (props) => {
-  const { name, carbohydrates, fats, calories, protein, image } = props.product;
+  const {
+    id,
+    name,
+    carbohydrates,
+    fats,
+    calories,
+    protein,
+    image,
+  } = props.product;
 
   return (
-    <>
-      <Card className="product-item">
-        <Card.Body>
-          <Card.Header className="text-center card-header" as="h5">
-            {<Translator getString={name} />}
-          </Card.Header>
-          <Card.Img
-            variant="bottom"
-            src={image}
-            style={{ marginBottom: "10px" }}
-          />
-          <Container fluid className="text-center products-info">
-            <Row>
-              <div className="col-6">
-                <div>{<Translator getString="Carbohydrates" />}</div>
-                <div>{carbohydrates}</div>
-              </div>
-              <div className="col-6">
-                <div>{<Translator getString="Fats" />}</div>
-                <div>{fats}</div>
-              </div>
-              <div className="col-6">
-                <div>{<Translator getString="Calories" />}</div>
-                <div>{calories}</div>
-              </div>
-              <div className="col-6">
-                <div>{<Translator getString="Proteins" />}</div>
-                <div>{protein}</div>
-              </div>
-            </Row>
-          </Container>
-        </Card.Body>
-      </Card>
-    </>
+    // <>
+    //   <Card className="product-item">
+    //     <Card.Body>
+    //       <Card.Header className="text-center card-header" as="h5">
+    //         {<Translator getString={name} />}
+    //       </Card.Header>
+    //       <Card.Img
+    //         variant="bottom"
+    //         src={image}
+    //         style={{ marginBottom: "10px" }}
+    //       />
+    //       <Container fluid className="text-center products-info">
+    //         <Row>
+    //           <div className="col-6">
+    //             <div>{<Translator getString="Carbohydrates" />}</div>
+    //             <div>{carbohydrates}</div>
+    //           </div>
+    //           <div className="col-6">
+    //             <div>{<Translator getString="Fats" />}</div>
+    //             <div>{fats}</div>
+    //           </div>
+    //           <div className="col-6">
+    //             <div>{<Translator getString="Calories" />}</div>
+    //             <div>{calories}</div>
+    //           </div>
+    //           <div className="col-6">
+    //             <div>{<Translator getString="Proteins" />}</div>
+    //             <div>{protein}</div>
+    //           </div>
+    //         </Row>
+    //       </Container>
+    //     </Card.Body>
+    //   </Card>
+    // </>
+
+    <div className="product">
+      <div className="rank">{id}</div>
+      <div className="front">
+        <img className="thumbnail" src={image} alt="" />
+        <h3 className="name">{<Translator getString={name} />}</h3>
+        <div className="stats">
+          <p className="receipts-count">5432</p>
+          <div className="receipts">
+            <img src={image} alt="" />
+            <img src={image} alt="" />
+            <img src={image} alt="" />
+          </div>
+        </div>
+      </div>
+
+      <div className="back">
+        <div className="product-info">
+          <p className="product-data-info">
+            <span>750</span>
+          </p>
+          <p className="product-data-info">
+            <span>750</span>
+          </p>
+        </div>
+        <a className="btn btn-primary btn-xl" href={`/receipts/${id}`}>
+          <Translator getString="Receipts with that product" />
+        </a>
+        <div className="receipts-back">
+          <div className="receipt">
+            <p className="name"></p>
+            <p className="number"></p>
+          </div>
+          <div className="receipt">
+            <p className="name"></p>
+            <p className="number"></p>
+          </div>
+          <div className="receipt">
+            <p className="name"></p>
+            <p className="number"></p>
+          </div>
+        </div>
+      </div>
+
+      <div className="background"></div>
+    </div>
   );
 };
 
